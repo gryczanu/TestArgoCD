f313cfe3e05f6e419e1fa13a94e1aee4c3813e92
gryczanu <52965471+gryczanu@users.noreply.github.com>
2020-10-14 14:37:45 +0200

    Update backup_localhost.sh

--- Mongo/backup_localhost.sh
+++ Mongo/backup_localhost.sh
@@ -13,4 +13,6 @@ FILE="/home/urszula/test/Mongo/backup-$DATE.tar.gz"
 mongodump --host $DATABASE_HOST:$DB_PORT --gzip --archive=$FILE
 curl -X PUT -T ./backup-$DATE.tar.gz -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://pmdevsa01.blob.core.windows.net/t-trade-desk/backup-$DATE.tar.gz?se=2020-12-31&sp=racwdl&sv=2018-11-09&sr=c&sig=3xUVQQMsAXLYQ67d6b44ukLdb6JOwrqbr/DgoQ3VEu8%3D"
 
+rm $FILE
+
 echo "backup created: $(date)"

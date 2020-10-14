b887ff479798c657348981ee1bce439704a90f47
gryczanu <52965471+gryczanu@users.noreply.github.com>
2020-10-14 14:16:48 +0200

    Update backup_localhost.sh

--- Mongo/backup_localhost.sh
+++ Mongo/backup_localhost.sh
@@ -10,6 +10,6 @@ DB_PORT=${DB_PORT:-27017}
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE="/home/urszula/test/Mongo/backup-$DATE.tar.gz"
 
-mongodump --host $DATABASE_HOST:$PORT --gzip --archive=$FILE
+mongodump --host $DATABASE_HOST:$DB_PORT --gzip --archive=$FILE
 
 echo "backup created: $(date)"

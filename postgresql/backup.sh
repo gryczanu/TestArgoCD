12296e9833dd96e56c51182fe51a6a102ad4902c
gryczanu <52965471+gryczanu@users.noreply.github.com>
2020-10-16 14:54:46 +0200

    Update backup.sh

--- postgresql/backup.sh
+++ postgresql/backup.sh
@@ -9,9 +9,8 @@ DATE=$(date +%Y%m%d_%H%M%S)
 FILE_NAME="backup-$DATE.sql.gz"
 FILE="/scripts/$FILE_NAME"
 
-
-pg_dump -h HOSTNAME | gzip > $FILE_NAME
-pg_dump -U POSTGRES_USER -h HOSTNAME | gzip > $FILE_NAME
+echo "$PGPASS" > /root/.pgpass && chmod 600 /root/.pgpass
+pg_dump -U $USERNAME -h $HOSTNAME $DATABASE| gzip > $FILE_NAME
 curl -X PUT -T ./$FILE_NAME -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://$STORAGE_ACCOUNT.blob.core.windows.net/$CONTAINER_NAME/$FILE_NAME?$SAS_TOKEN"
 
 rm $FILE

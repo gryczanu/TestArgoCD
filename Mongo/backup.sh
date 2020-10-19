7048f64a782b791f55eb54196e33b6a08db8c47e
gryczanu <52965471+gryczanu@users.noreply.github.com>
2020-10-19 08:36:24 +0200

    Update backup.sh

--- Mongo/backup.sh
+++ Mongo/backup.sh
@@ -6,7 +6,7 @@ echo "creating backup: $(date)"
 
 DATE=$(date +%Y%m%d_%H%M%S)
 FILE_NAME="backup-$DATE.tar.gz"
-FILE="/scripts/$FILE_NAME"
+FILE="/backups/$FILE_NAME"
 
 mongodump --uri=mongodb://$DATABASE_URI --gzip --archive=$FILE
 curl -X PUT -T ./$FILE_NAME -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://$STORAGE_ACCOUNT.blob.core.windows.net/$CONTAINER_NAME/$FILE_NAME?$SAS_TOKEN"
